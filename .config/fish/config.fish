ec4bda6f8348816169189f1fa8b692296c85432d
Dustin Lam <mynameisdustinlam@gmail.com>
2016-08-05 13:20:55 -0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,33 @@
 set fish_color_search_match --background=b294bb
 
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'no'
-set __fish_git_prompt_showuntrackedfiles 'no'
-set __fish_git_prompt_showupstream 'no'
+# Git colors
 set __fish_git_prompt_color_branch yellow
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
+# Git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'no'
+set __fish_git_prompt_showuntrackedfiles 'no'
+set __fish_git_prompt_showupstream 'no'
+set __fish_git_prompt_show_informative_status 'no'
+
 # Status Chars
-set __fish_git_prompt_char_dirtystate '+'
+set __fish_git_prompt_char_dirtystate '*'
 set __fish_git_prompt_char_stagedstate ''
-set __fish_git_prompt_char_untrackedfiles ''
+set __fish_git_prompt_char_invalidstate ''
+
 set __fish_git_prompt_char_stashstate ''
-set __fish_git_prompt_char_upstream_ahead ''
+
+set __fish_git_prompt_char_untrackedfiles ''
+
+set __fish_git_prompt_char_upstream_equal ''
 set __fish_git_prompt_char_upstream_behind ''
+set __fish_git_prompt_char_upstream_ahead ''
+set __fish_git_prompt_char_upstream_diverged ''
+
+set __fish_git_prompt_char_cleanstate ''
+set __fish_git_prompt_char_stateseparator ''
 
 function fish_prompt
   set last_status $status
