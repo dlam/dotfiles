c29cb42fc86fa53ce9f53de24d50f96bdb7ff69c
Dustin Lam <mynameisdustinlam@gmail.com>
2016-08-04 20:32:46 -0700

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+set fish_color_search_match --background=b294bb
+
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
@@ -8,12 +10,12 @@ set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_dirtystate '≠'
+set __fish_git_prompt_char_stagedstate '+'
+set __fish_git_prompt_char_untrackedfiles '-'
 set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
+set __fish_git_prompt_char_upstream_ahead '»'
+set __fish_git_prompt_char_upstream_behind '«'
 
 function fish_prompt
   set last_status $status
