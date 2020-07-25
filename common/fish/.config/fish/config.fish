b57928736952e92ee46cfc464cf4b92de99ba621
Dustin Lam <itsdustinlam+github@gmail.com>
2020-07-25 05:44:50 +0000

    Add more some env vars

--- common/fish/.config/fish/config.fish
+++ common/fish/.config/fish/config.fish
@@ -26,10 +26,11 @@
 #############################
 
 set -x EDITOR "/usr/bin/vim"
-set -x GPG_TTY "/usr/bin/tty"
+set -x GPG_TTY (tty)
 
 # $PATH
 set -x PATH "/snap/bin" $PATH
+set -x PATH "$HOME/go/bin" $PATH
 
 # Android
 set -x ANDROID_HOME "$HOME/Android/Sdk"
